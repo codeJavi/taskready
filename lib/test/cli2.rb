335b0ddb6def19f39813051b210363dbb3004ed6
Javi Leyva <jvrleyva@gmail.com>
2020-06-15 21:11:32 +0000

    first commit

---
+++ lib/test/cli2.rb
@@ -0,0 +1,8 @@
+require "test/cli2/version"
+
+module Test
+  module Cli2
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/test/cli2/version.rb
@@ -0,0 +1,5 @@
+module Test
+  module Cli2
+    VERSION = "0.1.0"
+  end
+end
