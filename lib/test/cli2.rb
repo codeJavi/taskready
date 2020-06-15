e9df14e5848cb3e160f055fae7b26fca5c099b72
Javi Leyva <jvrleyva@gmail.com>
2020-06-15 22:38:29 +0000

    added class to cli2.rb

--- lib/test/cli2.rb
+++ lib/test/cli2.rb
@@ -1,8 +1,5 @@
 require "test/cli2/version"
 
-module Test
-  module Cli2
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
+class greeting 
+  puts "Hello World"
+end 
